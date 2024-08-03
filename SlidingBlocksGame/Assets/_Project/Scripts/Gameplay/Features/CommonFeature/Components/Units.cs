09e77d19e2ddedbb5bf16108de1fb31f4165fde6
xtrayssss <pkneskill123@gmail.com>
2024-08-03 17:37:07 +0500

    Fixed max distance destination cell

--- SlidingBlocksGame/Assets/_Project/Scripts/Gameplay/Features/CommonFeature/Components/Units.cs
+++ SlidingBlocksGame/Assets/_Project/Scripts/Gameplay/Features/CommonFeature/Components/Units.cs
@@ -6,5 +6,6 @@ namespace _Project.Scripts.Gameplay.Features.CommonFeature.Components
     public struct Units : IEcsComponent
     {
         public EcsGroup Value;
+        public IEnumerable<entlong> Filtered;
     }
 }
